cf4999994659269ce3ab9a46232630ee394ac0f0
google-perfume <1834661238@qq.com>
2018-07-30 18:07:39 +0800

    Initial commit

---
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mycompany.myapp;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
